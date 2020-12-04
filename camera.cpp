a7fa657554d0f8c464d9d4f76340791999d6ab42
Loians <luyanantony@hotmail.com>
2020-12-04 00:19:15 -0300

    simplificacao da camera

--- camera.cpp
+++ camera.cpp
@@ -19,16 +19,16 @@ void Camera::update(float fElapsedTime, std::unordered_map<char, bool> keys, int
     float speed = 1.0f;
     glm::vec3 up(0.0f, 1.0f, 0.0f);
 
-    glm::vec3 camRight = glm::normalize(glm::cross(up, glm::vec3(look.x, 0.0f, look.z)));
+    glm::vec3 camRight = glm::normalize(glm::cross(look, up));
 
     if (keys['w'])
-        camPos += speed * fElapsedTime * glm::vec3(look.x, 0.0f, look.z);
+        camPos += speed * fElapsedTime * look;
     if (keys['s'])
-        camPos -= speed * fElapsedTime * glm::vec3(look.x, 0.0f, look.z);
+        camPos -= speed * fElapsedTime * look;
     if (keys['d'])
-        camPos -= speed * fElapsedTime * camRight;
-    if (keys['a'])
         camPos += speed * fElapsedTime * camRight;
+    if (keys['a'])
+        camPos -= speed * fElapsedTime * camRight;
     if (keys['q'])
         camPos.y += speed * fElapsedTime;
     if (keys['e'])
@@ -43,6 +43,6 @@ void Camera::update(float fElapsedTime, std::unordered_map<char, bool> keys, int
     if (mouseMove)
     {
         look = glm::rotate(glm::radians((float)(win_width / 2 - x) * 0.2f), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(look, 1.0f);
-        look = glm::rotate(glm::radians((float)(y - win_height / 2) * 0.2f), camRight) * glm::vec4(look, 1.0f);
+        look = glm::rotate(glm::radians((float)(win_height / 2 - y) * 0.2f), camRight) * glm::vec4(look, 1.0f);
     }
 }
